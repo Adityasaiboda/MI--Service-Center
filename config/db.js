1f865476c689f20fb47e1561283f4133a460eaaa
Aditya <adityasaiboda@gmail.com>
2026-06-10 16:45:09 +0530

    Added environment variables

--- config/db.js
+++ config/db.js
@@ -1,18 +1,35 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Aditya@123",
-    database: "mi_service_center"
+
+    host: process.env.DB_HOST,
+
+    port: process.env.DB_PORT,
+
+    user: process.env.DB_USER,
+
+    password: process.env.DB_PASSWORD,
+
+    database: process.env.DB_NAME
+
 });
 
-db.connect((err) => {
+db.connect((err)=>{
+
     if(err){
+
         console.log(err);
+
     }else{
-        console.log("Database Connected");
+
+        console.log(
+            "Database Connected"
+        );
+
     }
+
 });
 
 module.exports = db;
